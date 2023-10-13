0a44b5f0085e522aec6288435797c4b5a1a9ac03
sivacssankar <145976833+sivacssankar@users.noreply.github.com>
2023-10-13 11:57:39 +0530

    Untitled11.py

--- Untitled11.py
+++ Untitled11.py
@@ -1,19 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
 ab=int(input("enter current year"))
 bc=int(input("enter final year"))
 
-if(ab%400==0 or(ab%100 and ab%4==0)):
+if(ab%400==0 or ab%100 or ab%4==0):
     print("current year is a leap year",ab);
 print("the leap years are")
 for i in range(ab,bc):
-    if(i%400==0 or(ab%100 and i%4==0)):
+    if(i%400==0 or ab%100 or i%4==0):
         print(i)
-
 
 # In[ ]:
 
